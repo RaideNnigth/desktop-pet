985780dc34536ba7fce862430323809128884bbb
Gustavo Pereira <gustavo.pinzon55@gmail.com>
2025-08-31 13:45:19 -0300

    Chore: bootstrap Electron + React + Vite +Tailwind PET

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gradient-to-r from-sky-400 to-indigo-600">
+      <h1 className="text-4xl font-bold text-white">
+        🐾 Hello Desktop Pet
+      </h1>
+    </div>
+  )
+}
